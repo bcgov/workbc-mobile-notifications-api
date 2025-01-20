e4799940274cf6addd233fe2f3e17ebb89aa49f9
Maksym Kononenko <maks@Maksyms-MacBook-Pro.local>
2025-01-20 09:56:41 -0800

    make sure props are string

--- src/services/messaging.service.ts
+++ src/services/messaging.service.ts
@@ -2,34 +2,36 @@ import { getMessaging } from "firebase-admin/messaging";
 const firebaseApp = require("../config/firebase");
 
 type MessagePayload = {
-    token: string;
-    content: string;
-    title: string;
-    dryRun: boolean;
-    data: Record<string, string>;
-}
+  token: string;
+  content: string;
+  title: string;
+  dryRun: boolean;
+  data: Record<string, string>;
+};
 
 export const sendMessage = async (payload: MessagePayload): Promise<string> => {
-    try {
-        const { token, content, title, dryRun, data } = payload;
+  try {
+    const { token, content, title, dryRun, data } = payload;
 
-        const message = {
-            notification: {
-                body: content,
-                title: title
-            },
-            token: token,
-            data
-        };
+    const message = {
+      notification: {
+        body: content,
+        title: title,
+      },
+      token: token,
+      data: {
+        ...data,
+        ...('props' in data && { props: JSON.stringify(data.props) }),
+      },
+    };
 
-        // Send a message to the device corresponding to the provided
-        // registration token.
-        const resp = getMessaging(firebaseApp).send(message, dryRun);
-        //console.log(resp)
-        return resp;
-
-    } catch (error: any) {
-        console.log(error);
-        throw new Error(error);
-    }
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    const resp = getMessaging(firebaseApp).send(message, dryRun);
+    //console.log(resp)
+    return resp;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error);
+  }
 };
